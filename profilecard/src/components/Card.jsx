6ab97a6c232dda963e076edae9314b061e7cc1db
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2024-10-23 21:06:08 +0100

    applied props

--- profilecard/src/components/Card.jsx
+++ profilecard/src/components/Card.jsx
@@ -4,9 +4,9 @@ const Card =(props)=>{
     return(
         <div className='CardWrapper'>
             <img src={props.image} alt="" />
-            <h2>Name: {props.name}</h2>
-            <h3>Age: {props.age}</h3>
-            <h3>Location : {props.location}</h3>
+            <h4>Name: {props.name}</h4>
+            <h4>Age: {props.age}</h4>
+            <h4>Location : {props.location}</h4>
         </div>
     )
 }
