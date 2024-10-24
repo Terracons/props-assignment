933870d49a08976ad7906400f2cb94d1eb7c55ae
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2024-10-24 09:20:05 +0100

    updates on ecommerce page

--- ecompage/src/components/Card.jsx
+++ ecompage/src/components/Card.jsx
@@ -3,10 +3,15 @@ import './Card.css'
 const Card =(props)=>{
     return(
         <div className='CardWrapper'>
+            <div className="imgcontainer">
             <img src={props.image} alt="" />
-            <h4>Name: {props.name}</h4>
-            <h4>Age: {props.age}</h4>
-            <h4>Location : {props.location}</h4>
+            </div>
+            <div className="textcontainer">
+            <p>{props.category}</p>
+            <h4 className='name'> {props.name}</h4>
+            <h4 className='price'> ${props.price}</h4>
+            </div>
+
         </div>
     )
 }
--- ecompage/src/pages/Home.jsx
+++ ecompage/src/pages/Home.jsx
@@ -1,20 +1,29 @@
 import './Home.css'
 import Card from "../components/Card.jsx"
-import image1 from "../assets/img1.png"
-import image2 from "../assets/img2.png"
-import image3 from "../assets/img3.png"
-import image4 from "../assets/img4.png"
+import Waffle from "../assets/image-waffle-desktop.jpg"
+import image2 from "../assets/image-creme-brulee-desktop.jpg"
+import image3 from "../assets/image-macaron-desktop.jpg"
+import image4 from "../assets/image-tiramisu-desktop.jpg"
+import image5 from "../assets/image-baklava-desktop.jpg"
+import image6 from "../assets/image-meringue-desktop.jpg"
+import image7 from "../assets/image-cake-desktop.jpg"
+
 
 const Home =()=>{
     return(
         <div className='cardContainer'>
-        <Card  image = {image1} name = "Van dan" age="60" location = "Rusia"/>
-        <Card  image = {image2} name = "Andre Gas" age="54" location = "London"/>
-        <Card  image = {image3} name = "Awari Ajadi" age="52" location = "Ile -Ife"/>
-        <Card  image = {image4} name = "Adewale Ayinde" age="66" location = "Lagos,Nigeria"/>
+        <Card  image = {Waffle}  category= "Waffle" name = "Waffle with Berries"  price ="6.50"/>
+        <Card  image = {image2}  category= "Crème Brûlée" name = "Vanilla Bean Crème Brûlée"  price ="7.00"/>
+        <Card  image = {image3}  category= "Macaron" name = "Macaron Mix of Five"  price ="8.00"/>
+        <Card  image = {image4}  category= "Tiramisu" name = "Classic Tiramisu"  price ="5.50"/>
+        <Card  image = {image5}  category= "Baklava" name = "Pistachio Baklava"  price ="4.00"/>
+        <Card  image = {image6}  category= "Pie" name = "Lemon Meringue Pie"  price ="5.00"/>
+        <Card  image = {image7}  category= "Cake" name = "Red Velvet Cake"  price ="4.50"/>
         </div>
     )
     
 }
+
+
 
 export default Home;
